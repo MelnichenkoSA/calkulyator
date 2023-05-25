495700b8e5155e5f0e25c12d742b1aa52ec93e64
MelnichenkoSA <MelnichenkoSA@302D-5>
2023-05-25 12:11:13 +0700

    Добавьте файлы проекта.

---
+++ calkulyator/Operations.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calkulyator
+{
+    class Operations
+    {
+        public double Calculate(string expression)
+        {
+            double result = 0;
+            string[] ops = expression.Split('+', '-', '*', '/');
+            double firstp = double.Parse(ops[0]);
+            double secondp = double.Parse(ops[1]);
+            char op = expression[ops[0].Length];
+
+            switch (op)
+            {
+                case '+': result = firstp + secondp; break;
+                case '-': result = firstp - secondp; break;
+                case '*': result = firstp * secondp; break;
+                case '/': result = firstp / secondp; break;
+            }
+
+            return result;
+        }
+    }
+}
